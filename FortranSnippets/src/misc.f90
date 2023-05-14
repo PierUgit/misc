d23c68bfccc8ba9d5c74dd4673fde72c88f8badc
PierUgit <comptes@ugo235.fr>
2023-05-14 10:36:40 +0200

    fixed formatting

--- FortranSnippets/src/misc.f90
+++ FortranSnippets/src/misc.f90
@@ -34,42 +34,42 @@ real, pointer :: a1(:,:), a2(:,:)
 
 ! trivial square matrix case
 if (n == m) then
-	a1(1:n,1:m) => a
-	a2(1:m,1:n) => a
-    do j2 = 1, n
-    	do i2 = 1, j2-1
-    		t = a2(i2,j2)
-    		a2(i2,j2) = a1(j2,i2)
-    		a1(j2,i2) = t
-        end do
-    end do
-    return
+   a1(1:n,1:m) => a
+   a2(1:m,1:n) => a
+   do j2 = 1, n
+      do i2 = 1, j2-1
+         t = a2(i2,j2)
+         a2(i2,j2) = a1(j2,i2)
+         a1(j2,i2) = t
+      end do
+   end do
+   return
 end if
 
 call b%allocate(n*m)
 call b%setall(.false.)
 
 do k = 1, n*m
-    if (b%get(k)) cycle   ! element already updated
-    ! start of a new cycle at element k
-    t = a(k)              ! keep for later
-    call b%set(k,.true.)  ! mark as "being updated"
-    k2 = k
-    do
-        j2 = (k2-1)/m + 1    ! indexes in the destination matrix
-        i2 = k2 - (j2-1)*m   !
-        i1 = j2              ! indexes in the source matrix
-        j1 = i2              !
-        k1 = (j1-1)*n + i1   ! position of the source element in the 1D array
-        if (b%get(k1)) then  
-            ! if the source element is marked, then it was the first one of the cycle
-            a(k2) = t   ! upadte the destination with the value that had been stored 
-            exit   ! end of the cycle
-        end if
-        a(k2) = a(k1)           ! update destination 
-        call b%set(k1,.true.)   ! mark source as used
-        k2 = k1
-    end do
+   if (b%get(k)) cycle   ! element already updated
+   ! start of a new cycle at element k
+   t = a(k)              ! keep for later
+   call b%set(k,.true.)  ! mark as "being updated"
+   k2 = k
+   do
+      j2 = (k2-1)/m + 1    ! indexes in the destination matrix
+      i2 = k2 - (j2-1)*m   !
+      i1 = j2              ! indexes in the source matrix
+      j1 = i2              !
+      k1 = (j1-1)*n + i1   ! position of the source element in the 1D array
+      if (b%get(k1)) then  
+         ! if the source element is marked, then it was the first one of the cycle
+         a(k2) = t   ! upadte the destination with the value that had been stored 
+         exit   ! end of the cycle
+      end if
+      a(k2) = a(k1)           ! update destination 
+      call b%set(k1,.true.)   ! mark source as used
+      k2 = k1
+   end do
 end do
 
 call b%deallocate()
