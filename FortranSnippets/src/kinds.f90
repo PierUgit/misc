131f1345ec6729874eb486c85cd11f411ace1baa
PierUgit <comptes@ugo235.fr>
2024-11-11 03:01:54 +0100

    updated kinds

--- FortranSnippets/src/kinds.f90
+++ FortranSnippets/src/kinds.f90
@@ -3,159 +3,151 @@
 ! https://github.com/PierUgit
 ! License: GPL v3 
 !***********************************************************************************************
-! (hopefully) useful real and integer kind values
+! # Yet Another KINDS
 !
-! For the real type the module defines the following kinds:
-! - rk_default and rk_doubleprecision for the default real and "double precision"
-! - rk_single       (single precision):        precision >= 6,  range >= 37
-! - rk_weakdouble   (weak double precision):   precision >= 10, range >= 37
-! - rk_double       (double precision):        precision >= 12, range >= 307  (*)
-! - rk_extended     (extened precision):       precision >= 18, range >= 100  (*)
-! - rk_doubledouble (double double precision): precision >= 24, range >= 100  (*)
-! - rk_quad         (quadruple precision):     precision >= 24, range >= 1000 (*)
+! The module provides REAL / INTEGER / LOGICAL kind constants, based on three principles:
+! - The kinds are based on precision and range requirements, without any reference
+!   to the sizes in memory (apart for the LOGICAL kinds), so that the codes can focus
+!   on the numerical properties only.
+! - The default kinds (kind(0.0), kind(0d0), kind(0), kind(.false.)) are given priority
+!   as long as they fit the requirements, so that it maximizes the compatibility with the
+!   legacy librairies that typically use these kinds
+! - The kinds are always defined (>0): if the requirements for a kind cannot be
+!   met, the kind with the requirements that are just below is selected.  
+!   The advantage of this approach is that codes that use these kinds can always 
+!   be compiled. The downside is that one has to test the actual precision/range at 
+!   runtime and make a decision.
+!
+! ## REAL kind constants
+! - r_default : the default real (kind(1.0))
+! - r_doubleprecision : the "double precision" real (kind(1d0))
+! - r_single       (single precision):        precision >= 6,  range >= 37    **
+! - r_sesqui       (1.5x precision):          precision >= 10, range >= 37    **
+! - r_double       (double precision):        precision >= 12, range >= 100   * x
+! - r_extended     (extended precision):      precision >= 18, range >= 100   * x
+! - r_doubledouble (double-double precision): precision >= 24, range >= 100     x
+! - r_quad         (quadruple precision):     precision >= 24, range >= 1000    x
 ! 
-! (*) note that these kinds are always defined, even if the requirements are not met.
-!     rk_double  falls back to rk_weakdouble if no kind meets the requirements
-!     rk_extended  falls back to rk_double  if no kind meets the requirements
-!     rk_doubledouble falls back to rk_extended  if no kind meets the requirements
-!     rk_quad  falls back to rk_doubledouble if no kind meets the requirements
-!     The advantage of this approach is that codes that use these kind constants will always 
-!     compile. The downside is that one has to test the actual precision/range and 
-!     take a decision at the execution. To make it easier, the module also provides the
-!     constants rk_double_prec,  rk_double_range,  rk_extended_prec, rk_extended_range, 
-!               rk_doubledouble_prec, rk_doubledouble_range, rk_quad_prec, rk_quad_range
+! ** : these kinds are necessarily either r_default or r_doubleprecision
+! * : these kinds are r_default or r_doubleprecision in priority (if possible)
+! x : no guarantee to meet the requirements
+!
+! Note that Fortran standard:
+! - *recommends* precision(0.0) >= 6,  range(0.0) >= 37
+! - *requires*   precision(0d0) >= 10, range(0d0) >= 37
+!
+! ### Fallbacks
+! - r_double  falls back to r_sesqui if the requirements are not met
+! - r_extended  falls back to r_double if the requirements are not met
+! - r_doubledouble falls back to r_extended if the requirements are not met
+! - r_quad  falls back to r_doubledouble if the requirements are not met
 !     
-! Note that Fortran standard only *recommends* precision(0.0) >= 6,  range(0.0) >= 37
-!                             but *requires*   precision(0d0) >= 10, range(0d0) >= 37
-! Consequentely, rk_single and rk_weakdouble always meet the requirements 
-! and are *always* either rk_default or rk_doubleprecision
+! ## INTEGER kind constants
+! - i_default : the default integer (kind(0))
+! - i_shorter :         range >= 2
+! - i_short :           range >= 4
+! - i_medium :          range >= 9    *
+! - i_long :            range >= 18   *
+! - i_longer :          range >= 36     x
 !
-! It also provides alternatives to the kinds real32 and real64, the difference being that
-! these kinds have likely *at least* (instead of exactly) 32 or 64 bits
-! - rk_sortof32
-! - rk_sortof64
+! * : these kinds are i_default in priority (if possible)
+! x : no guarantee to meet the requirements
 !
-! For the integer type the module provides the kinds
-! - ik_default (the default kind)
-! - ik_long, which is 
-!   - the default kind if it has a range >= 18
-!   - selected_int_kind(r=18) otherwise
-! - ik_longer, which is
-!   - selected_int_kind(r=36) is it exists
-!   - ik_long otherwise
+! Note that the standard requires the compilers to provide an integer with a range >= 18.
+! Consequently, only i_longer is not guaranteed to meet the requirements (in that case, 
+! it falls back to i_long).
 !
-! Note that the standard requires the compilers to provide an integer with a range >= 18
-!
-! It also provides alternatives to the kinds int32 and int64, the difference being that
-! these kinds have likely *at least* (instead of exactly) 32 or 64 bits
-! - ik_sortof32
-! - ik_sortof64
-!
-! For the logical type the module provides the kinds
-! - lk_default (the default kind)
-! - lk_short, which is currently the c_bool kind from the iso_c_binding module
-! - lk_shortest (not yet)
+! ## LOGICAL kind constants
+! - l_default (the default kind)
+! - l_short, which is currently the c_bool kind from the iso_c_binding module
 !***********************************************************************************************
-module tortuous_kinds
+module yakinds
 use iso_c_binding, only : c_bool
 implicit none
 
    private
 
    integer, parameter ::  sp_pmin = 6,   sp_rmin = 37,   &
-                         wdp_pmin = 10, wdp_rmin = 37,  &
+                         sep_pmin = 10, sep_rmin = 37,   &
                           dp_pmin = 12,  dp_rmin = 100,  &
                           ep_pmin = 18,  ep_rmin = 100,  &
                          ddp_pmin = 24, ddp_rmin = 100,  &
                           qp_pmin = 24,  qp_rmin = 1000
 
-   integer, parameter :: rk_default = kind(0.0), &
-                         rk_doubleprecision = kind(0d0)
+   integer, parameter :: r_default = kind(0.0), &
+                         r_doubleprecision = kind(0d0)
 
-   ! determining rk_single
+   ! determining r_single
    logical, parameter :: &
-      is_default_least_sp = precision(0.0) >= sp_pmin .and. range(0.0) >= sp_rmin
-   integer, parameter :: rk_single = merge(rk_default,rk_doubleprecision,is_default_least_sp)
+      is_def_least_sp = precision(0.0) >= sp_pmin .and. range(0.0) >= sp_rmin
+   integer, parameter :: r_single = merge(r_default,r_doubleprecision,is_def_least_sp)
 
-   ! determining rk_weakdouble
+   ! determining r_sesqui
    logical, parameter :: &
-      is_def_least_wdp = precision(0.0) >= wdp_pmin .and. range(0.0) >= wdp_rmin
-   integer, parameter ::                                       &
-      rk_weakdouble = merge(rk_default,rk_doubleprecision,is_def_least_wdp), &
-      rk_weakdouble_prec = precision(0.0_rk_weakdouble),                     &
-      rk_weakdouble_range = range(0.0_rk_weakdouble)
+      is_def_least_sep = precision(0.0) >= sep_pmin .and. range(0.0) >= sep_rmin
+   integer, parameter :: r_sesqui = merge(r_default,r_doubleprecision,is_def_least_sep)
     
-   ! determining rk_double
+   ! determining r_double
    logical, parameter :: &
-      is_wdp_least_dp = rk_weakdouble_prec >= dp_pmin .and. rk_weakdouble_range >= dp_rmin
-   integer, parameter ::                                        &
-      dp___ = selected_real_kind(p=dp_pmin,r=dp_rmin),          &
-      rk_double = merge(rk_weakdouble,dp___,is_wdp_least_dp .or. dp___<0), &
-      rk_double_prec  = precision(0.0_rk_double),                       &
-      rk_double_range = range(0.0_rk_double)
+      is_sep_least_dp = precision(0.0_r_sesqui) >= dp_pmin .and. &
+                        range(0.0_r_sesqui) >= dp_rmin
+   integer, parameter ::                                             &
+      dp___ = selected_real_kind(p=dp_pmin,r=dp_rmin),               &
+      r_double = merge(r_sesqui,dp___,is_sep_least_dp .or. dp___<0)
 
-   ! determining rk_extended
+   ! determining r_extended
    logical, parameter :: &
-      is_dp_least_ep = rk_double_prec >= ep_pmin .and. rk_double_range >= ep_rmin
-   integer, parameter ::                                      &
-      ep___ = selected_real_kind(p=18,r=100),                 &
-      rk_extended = merge(rk_double,ep___,is_dp_least_ep .or. ep___<0), &
-      rk_extended_prec  = precision(0.0_rk_extended),                     &
-      rk_extended_range = range(0.0_rk_extended)
+      is_dp_least_ep = precision(0.0_r_double) >= ep_pmin .and. &
+                       range(0.0_r_double) >= ep_rmin
+   integer, parameter ::                                              &
+      ep___ = selected_real_kind(p=18,r=100),                         &
+      r_extended = merge(r_double,ep___,is_dp_least_ep .or. ep___<0)
 
-   ! determining rk_doubledouble
-   integer, parameter ::                                  &
-      ddp___ = selected_real_kind(p=ddp_pmin,r=ddp_rmin), &
-      rk_doubledouble = merge(ddp___,rk_extended,ddp___>0),              &
-      rk_doubledouble_prec  = precision(0.0_rk_doubledouble),               &
-      rk_doubledouble_range = range(0.0_rk_doubledouble)
+   ! determining r_doubledouble
+   integer, parameter ::                                     &
+      ddp___ = selected_real_kind(p=ddp_pmin,r=ddp_rmin),    &
+      r_doubledouble = merge(ddp___,r_extended,ddp___>0)
 
-   ! determining rk_quad
+   ! determining r_quad
    integer, parameter ::                                &
       qp___  = selected_real_kind(p=qp_pmin,r=qp_rmin), &
-      rk_quad  = merge(qp___,rk_doubledouble,qp___>0),             &
-      rk_quad_prec  = precision(0.0_rk_quad),               &
-      rk_quad_range = range(0.0_rk_quad)
+      r_quad  = merge(qp___,r_doubledouble,qp___>0)
 
-   public :: rk_default, rk_doubleprecision
-   public :: rk_single, rk_weakdouble, rk_double, rk_extended, rk_doubledouble, rk_quad
-   public :: rk_double_prec, rk_double_range,   &
-             rk_extended_prec, rk_extended_range,   &
-             rk_doubledouble_prec, rk_doubledouble_range, &
-             rk_quad_prec, rk_quad_range
-
-   integer, parameter :: rk_sortof32 = rk_single
-   integer, parameter :: rk_sortof64 = rk_double
-
-   public :: rk_sortof32, rk_sortof64
+   public :: r_default, r_doubleprecision
+   public :: r_single, r_sesqui, r_double, r_extended, r_doubledouble, r_quad
 
 
 
-   integer, parameter :: ik_default = kind(0)
+   integer, parameter :: i_default = kind(0)
+   
+   ! determining i_xshort
+   integer, parameter :: i_shorter = selected_int_kind(r=2)
+   
+   ! determining i_short
+   integer, parameter :: i_short = selected_int_kind(r=4)
 
-   ! determining ik_long
+   ! determining i_medium
+   logical, parameter :: is_def_least9 = range(0) >= 9
+   integer, parameter :: medium___ = selected_int_kind(r=9)
+   integer, parameter :: i_medium = merge(kind(0),medium___,is_def_least9)
+
+   ! determining i_long
    logical, parameter :: is_def_least18 = range(0) >= 18
-   integer, parameter :: ik_long___ = selected_int_kind(r=18)
-   integer, parameter :: ik_long = merge(kind(0),ik_long___,is_def_least18)
-   integer, parameter :: ik_long_range = range(0_ik_long)
+   integer, parameter :: long___ = selected_int_kind(r=18)
+   integer, parameter :: i_long = merge(kind(0),long___,is_def_least18)
 
-   ! determining ik_longer
-   integer, parameter :: ik_longer___ = selected_int_kind(r=36)
-   integer, parameter :: ik_longer = merge(ik_longer___,ik_long,ik_longer___>0)
-   integer, parameter :: ik_longer_range = range(0_ik_longer)
+   ! determining i_longer
+   integer, parameter :: longer___ = selected_int_kind(r=36)
+   integer, parameter :: i_longer = merge(longer___,i_long,longer___>0)
 
-   public :: ik_default, ik_long, ik_longer, ik_long_range, ik_longer_range
-
-   integer, parameter :: ik_sortof32 = selected_int_kind(r=9)
-   integer, parameter :: ik_sortof64 = ik_long
-
-   public :: ik_sortof32, ik_sortof64
+   public :: i_default, i_shorter, i_short, i_medium, i_long, i_longer 
 
 
-   integer, parameter :: lk_default = kind(.true.)
-   integer, parameter :: lk_short   = c_bool
 
-   public :: lk_default, lk_short
+   integer, parameter :: l_default = kind(.true.)
+   integer, parameter :: l_short   = c_bool
 
-end module tortuous_kinds
+   public :: l_default, l_short
+
+end module yakinds
 
